5b6dac587c69937689e018132bbff1a76c38f324
Котылев Ярослав <yaroslav@MacBook-Pro-Aroslav.local>
2019-12-17 22:24:06 +0300

    updates

--- Dev/src/sample/database/Getter.java
+++ Dev/src/sample/database/Getter.java
@@ -16,20 +16,14 @@ public class Getter {
     }
 
     public Result getFilteredPublications(String title, String author, String from, String to, String types) {
-        String query = queryString(title, author, from, to, types);
-
-        System.out.println(query);
-        return dbWorker.query(query);
-    }
-
-    private String queryString(String title, String author, String from, String to, String types) {
-        String query = dbWorker.readLineByLineJava8("Filter.xq");
+        String fileLines = dbWorker.readLineByLineJava8("Filter.xq");
         if (from.isEmpty()) {
             from = "0";
         }
         if (to.isEmpty()) {
             to = "0";
         }
-        return String.format(query, title, author, from, to, types);
+        String query = String.format(fileLines, title, author, from, to, types);
+        return dbWorker.query(query);
     }
 }
